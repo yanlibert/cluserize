94ff3d3cb20b1ebb6afb63b89179d3629724ad57
yanlibert <yannick.libert+github@gmail.com>
2018-04-27 09:26:59 +0200

    Updated clusterize

--- clusterize.sh
+++ clusterize.sh
@@ -4,10 +4,20 @@
 # provisioned K8s cluster nodes using a
 # list of available hosts with their ip
 # addresses on the local network.
-# 
+#
+# Checking for compatible shell version
+#
+SHELL_VERSION=$(bash --version | grep version | head -1 | awk '{print $4}' | cut -d\. -f1)
+#
+if [ $SHELL_VERSION -lt 4 ]
+then
+    echo "bash version is $SHELL_VERSION"
+    echo "clusterize is compatible with version 4 and greater"
+    exit 1
+fi
+#
 # The ip of the nodes should be collected
 # in a file named ip_list.txt
-#
 #
 if [ ! -f "ip_list.txt" ]
 then
@@ -16,11 +26,13 @@ then
     exit 1
 fi
 #
-list=$(cat ip_list.txt)
+ip_list=$(cat ip_list.txt)
 #
 # Associate a hostame with the ip
+# and storing it in an associative array
 #
 i=1
+declare -A host_names
 #
 touch /tmp/ip_host.txt
 if [ ! -f "/tmp/ip_host.txt" ]
@@ -31,10 +43,24 @@ else
     touch /tmp/ip_host.txt
 fi
 #
-for machine in $list
+for ip in $ip_list
 do
     hostname="$(head -$i ./dic/SA_animals.dic | tail -1)"
-    echo "$machine => $hostname" >> /tmp/ip_host.txt
+    host_names[$ip]=$hostname
+    echo "$ip => $hostname" >> /tmp/ip_host.txt
     i=$((i+1))
 done
 #
+# Starting from scratch
+#
+if [ -d "./hosts" ]
+then
+    rm -f hosts/*
+else
+    mkdir hosts
+fi
+#
+for ip in "${!host_names[@]}"
+do
+    mkdir ./hosts/"${host_names[$ip]}"
+done
